53b2b4427567801f66cc3ee541d289138d01c9e3
Mayara Lupeti <mayaralupetiturbiani@gmail.com>
2019-09-19 17:32:17 -0300

    Exercícios do dia 19/09

---
+++ empresa/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace empresa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           double sal;
+           double newsal;
+           double aumento = 0.3;
+           Console.WriteLine("Digite seu salário:");
+           sal = double.Parse(Console.ReadLine());
+           
+           newsal = (aumento * sal) + sal;
+
+           if (sal < 500) {
+               Console.WriteLine("Seu salário atual é " + newsal);
+           } else if (sal > 500) {
+               Console.WriteLine("Não houve alterações, seu salário continua sendo " + sal);
+           }
+
+           
+        }
+    }
+}
---
+++ media/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace media
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float num1 = 0.0f;
+            float num2 = 0.0f;
+            float num3 = 0.0f;
+            float num4 = 0.0f;
+            double media;
+            
+            Console.WriteLine("Digite a 1° nota:");
+            num1 = float.Parse(Console.ReadLine());
+            Console.WriteLine("Digite a 2° nota:");
+            num2 = float.Parse(Console.ReadLine());
+            Console.WriteLine("Digite a 3° nota:");
+            num3 = float.Parse(Console.ReadLine());
+            Console.WriteLine("Digite a 4° nota:");
+            num4 = float.Parse(Console.ReadLine());
+
+            media = (num1 + num2 + num3 + num4) /4;
+
+            Console.WriteLine("Sua média é " + media);
+
+            if (media >= 7) {
+                Console.WriteLine("Aprovado!");
+            } else if (media < 7) {
+                Console.WriteLine("Retido");
+            }
+
+            
+        }
+    }
+}
