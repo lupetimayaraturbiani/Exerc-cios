e9fe37c768b1326c6e9bf1ce334e38adea2a8d39
Mayara Lupeti <mayaralupetiturbiani@gmail.com>
2019-09-20 17:32:20 -0300

    exercícios dia 20/09

---
+++ aplicação/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace aplicação
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   int numx = 1;
+            
+            while (numx != 0) {
+            Console.WriteLine("Escreva um número inteiro ou zero para parar ");
+            numx = int.Parse(Console.ReadLine());
+            
+            if (numx % 2 == 0) {
+                Console.WriteLine("Esse número é par");
+            } else {
+                Console.WriteLine("Esse número é ímpar");
+            }
+            }
+        }
+    }
+}
