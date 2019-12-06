027097e574b5a0a0e82272db13647cf8b8fbe5d5
Mayara Lupeti <mayaralupetiturbiani@gmail.com>
2019-12-06 17:30:42 -0300

    pastas dia 06/12

--- RoleTopMVC/Controllers/UsuarioController.cs
+++ RoleTopMVC/Controllers/UsuarioController.cs
@@ -1,7 +1,99 @@
+using System;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RoleTopMVC.Models;
+using RoleTopMVC.Repositories;
+using RoleTopMVC.ViewModels;
+
 namespace RoleTopMVC.Controllers
 {
     public class UsuarioController : AbstractController
     {
-        
+        ClienteRepository clienteRepository = new ClienteRepository();
+        EventoRepository eventoRepository = new EventoRepository();
+        TiposEventoRepository tiposEventoRepository = new TiposEventoRepository();
+        ServicosAdicionaisRepository servicosAdicionaisRepository = new ServicosAdicionaisRepository();
+
+        public IActionResult Usuario()
+        {
+            UsuarioViewModel uvm = new UsuarioViewModel();
+            uvm.TiposEvento = tiposEventoRepository.ObterTodos();
+            uvm.ServicosAdicionais = servicosAdicionaisRepository.ObterTodos();
+
+            var emailCliente = ObterUsuarioSession();
+            if (!string.IsNullOrEmpty(emailCliente))
+            {
+                uvm.Cliente = clienteRepository.ObterPor(emailCliente);
+            }
+
+            var nomeUsuario = ObterUsuarioNomeSession();
+            if (!string.IsNullOrEmpty(nomeUsuario))
+            {
+                uvm.NomeCliente = nomeUsuario;
+            }
+
+            var usuarioLogado = ObterUsuarioSession();
+            var nomeUsuarioLogado = ObterUsuarioNomeSession();
+            if (!string.IsNullOrEmpty(nomeUsuarioLogado))
+            {
+                uvm.UsuarioNome = nomeUsuarioLogado;
+            }
+
+            var clienteLogado = clienteRepository.ObterPor(usuarioLogado);
+            if (clienteLogado != null)
+            {
+                uvm.Cliente = clienteLogado;
+            }
+
+            uvm.NomeView = "Evento";
+            uvm.UsuarioEmail = ObterUsuarioSession();
+            uvm.UsuarioNome = ObterUsuarioNomeSession();
+
+
+            return View(uvm);
+        }
+
+        public IActionResult Registrar (IFormCollection form)
+        {
+            ViewData["Action"] = "Usuario";
+
+            Evento evento = new Evento();
+
+            var nomeTiposEvento = form["tipos_evento"];
+            TiposEvento tiposEvento = new TiposEvento(nomeTiposEvento, tiposEventoRepository.ObterPrecoDe(nomeTiposEvento));
+
+            evento.TiposEvento = tiposEvento;
+
+            var nomeServicosAdicionais = form["servicos_adicionais"];
+            ServicosAdicionais servicosAdicionais = new ServicosAdicionais(nomeServicosAdicionais, servicosAdicionaisRepository.ObterPrecoDe(nomeServicosAdicionais));
+
+            evento.ServicosAdicionais = servicosAdicionais;
+
+            Cliente cliente = new Cliente()
+            {
+                Nome = form["nome"],
+                Email = form["email"],
+                Telefone = form["telefone"],
+            };
+
+            evento.Cliente = cliente;
+
+            evento.DataEvento = DateTime.Now;
+
+            evento.PrecoTotal = tiposEvento.Preco + servicosAdicionais.Preco;
+
+
+            if (eventoRepository.Inserir(evento))
+            {
+                return View("Sucesso", new RespostaViewModel(){
+                    UsuarioEmail = ObterUsuarioSession(),
+                    UsuarioNome = ObterUsuarioNomeSession()
+                });
+            }
+            else
+            {
+                return View("Erro");
+            }
+        }
     }
 }
--- RoleTopMVC/ViewModels/UsuarioViewModel.cs
+++ RoleTopMVC/ViewModels/UsuarioViewModel.cs
@@ -6,5 +6,20 @@ namespace RoleTopMVC.ViewModels
     public class UsuarioViewModel : BaseViewModel
     {
         public List<Evento> Eventos {get;set;}
+        public List<TiposEvento> TiposEvento {get;set;}
+        public List<ServicosAdicionais> ServicosAdicionais {get;set;}
+        public Cliente Cliente {get;set;}
+        public string NomeCliente {get;set;}
+
+
+// ACRESCENTAR A SUBSTITUIÇÃO DO NOME COMPLETO DO CLIENTE POR NOMECLIENTE//
+        public UsuarioViewModel()
+        {
+            this.Eventos = new List<Evento>();
+            this.TiposEvento = new List<TiposEvento>();
+            this.ServicosAdicionais = new List<ServicosAdicionais>();
+            this.Cliente = new Cliente();
+            this.NomeCliente = "Nome Completo";
+        }
     }
 }
